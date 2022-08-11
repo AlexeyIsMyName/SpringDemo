8c693b1ab23b90a1540ad03a3e71cc46c6044bd0
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-08-11 23:23:46 +0300

    Added Spring framework, prepared project for the making Animation Model as part of MVC Pattern.

--- SpringDemo/Controller/ViewController.swift
+++ SpringDemo/Controller/ViewController.swift
@@ -5,15 +5,21 @@
 //  Created by ALEKSEY SUSLOV on 11.08.2022.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var animationView: SpringView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    @IBAction func runAnimationButtonPressed(_ sender: UIButton) {
+        
+    }
+    
 
 }
 
